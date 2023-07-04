698c88cb56e14ef11675e71b6062e9575bd33494
tetta maeda <nu.maeda@gmail.com>
2023-07-04 14:49:27 +0900

    Revert "feat: echo off"
    This reverts commit fce7d4a862ae1deb6797a8cbb4dc4f716015da33.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import createRNNWasmModule from './rnnoise.js';
-import RingBuffer from '../audio-utils/src/ringBuffer.js';
 
 (async () => {
 
@@ -16,9 +15,6 @@ import RingBuffer from '../audio-utils/src/ringBuffer.js';
   const pcmInputIndex = pcmInputBuf / 4;
   const pcmOutputIndex = pcmOutputBuf / 4;
 
-  const inputRingBuffer = new RingBuffer(INPUT_BUFFER_FRAMES * FRAME_SIZE, Float32Array);
-  const outputRingBuffer = new RingBuffer(INPUT_BUFFER_FRAMES * FRAME_SIZE, Float32Array);
-
   const audioElm = document.getElementById("my-audio");
 
   // MediaStream取得とInsertableStream用processor
@@ -26,8 +22,6 @@ import RingBuffer from '../audio-utils/src/ringBuffer.js';
     video: false, 
     audio: {
       noiseSuppression: false,
-      echoCancellation: false, 
-      
     }})
     .then((stream) => {
       return stream;
@@ -42,7 +36,6 @@ import RingBuffer from '../audio-utils/src/ringBuffer.js';
 
   const denoisedStream = new MediaStream();
   denoisedStream.addTrack(generator);
-
 
   //audioElm.srcObject = stream;
 
@@ -71,74 +64,60 @@ import RingBuffer from '../audio-utils/src/ringBuffer.js';
       const audioByteLength = await inputAudioData.numberOfFrames;
 
       const inputArray = new Float32Array(audioByteLength);
-      const denormalizedInputArray = new Float32Array(FRAME_SIZE);
-      const outputArray = new Float32Array(FRAME_SIZE);
-      const normalizedOutputArray = new Float32Array(FRAME_SIZE);
+      const denormalizedInputArray = new Float32Array(inputArray.length);
+      const outputArray = new Float32Array(inputArray.length);
+      const normalizedOutputArray = new Float32Array(inputArray.length);
 
       inputAudioData.copyTo(inputArray, {planeIndex: 0});
-      inputRingBuffer.write(inputArray);
-      let denoisingArray;
 
-      if(denoisingArray = inputRingBuffer.read(FRAME_SIZE)){
-
-        // 非正規化
-        // AudioDataの値の範囲は -1 〜 1
-        // RNNoiseの値の範囲は -32767 〜 32767
-        for (let i = 0; i < denoisingArray.length; i++) {
-          denormalizedInputArray[i] = denoisingArray[i] * 0x7FFF; // Multiply by 2^15-1 (32767) to Int16 range
-        }
-
-        const start = performance.now();
-
-        wasmModule.HEAPF32.set(denormalizedInputArray, pcmInputIndex);
-        await wasmModule._rnnoise_process_frame(denoiser, pcmOutputBuf, pcmInputBuf)
-
-        const denoisedFrame = wasmModule.HEAPF32.subarray(pcmOutputIndex, pcmOutputIndex + FRAME_SIZE);
-        outputArray.set(denoisedFrame, 0);
-
-        const end = performance.now();
-        
-        totalTime = totalTime + (end - start);
-        counter++;
-
-        if (counter % 1000 == 0){
-          console.log("Average process time: " + (totalTime / counter) + "[ms]");
-          counter = 0;
-          totalTime = 0.0;
-        }
-
-        // console.log("start: " + start + "  end: " + end + "  total: " + (end - start));
-
-
-        // 正規化
-        // AudioDataの値の範囲は -1 〜 1
-        // RNNoiseの値の範囲は -32767 〜 32767
-        for (let i = 0; i < outputArray.length; i++) {
-          normalizedOutputArray[i] = outputArray[i] / 0x7FFF;
-        }
-
-        outputRingBuffer.write(normalizedOutputArray);
-
+      // 非正規化
+      // AudioDataの値の範囲は -1 〜 1
+      // RNNoiseの値の範囲は -32767 〜 32767
+      for (let i = 0; i < inputArray.length; i++) {
+        denormalizedInputArray[i] = inputArray[i] * 0x7FFF; // Multiply by 2^15-1 (32767) to Int16 range
       }
 
-      let denoisedArray;
+      const start = performance.now();
 
-      while(denoisedArray = outputRingBuffer.read(inputAudioData.numberOfFrames)){
+      wasmModule.HEAPF32.set(denormalizedInputArray, pcmInputIndex);
+      await wasmModule._rnnoise_process_frame(denoiser, pcmOutputBuf, pcmInputBuf)
 
-        // [Chrome] sampleRate: 48000, numberOfFrames: 480, format: `f32-planar`
-        // Insertable Streamsの対応ブラウザが増えたら可変にする必要がある
-        const format = 'f32-planar';
-        let outputAudioData = new AudioData({
-          format,
-          sampleRate: inputAudioData.sampleRate,
-          numberOfFrames: inputAudioData.numberOfFrames,
-          numberOfChannels: inputAudioData.numberOfChannels,
-          timestamp: inputAudioData.timestamp,
-          data: denoisedArray,
-        });
-        controller.enqueue(outputAudioData);
+      const denoisedFrame = wasmModule.HEAPF32.subarray(pcmOutputIndex, pcmOutputIndex + FRAME_SIZE);
+      outputArray.set(denoisedFrame, 0);
 
+      const end = performance.now();
+      
+      totalTime = totalTime + (end - start);
+      counter++;
+
+      if (counter % 1000 == 0){
+        console.log("Average process time: " + (totalTime / counter) + "[ms]");
+        counter = 0;
+        totalTime = 0.0;
       }
+
+      // console.log("start: " + start + "  end: " + end + "  total: " + (end - start));
+
+
+      // 正規化
+      // AudioDataの値の範囲は -1 〜 1
+      // RNNoiseの値の範囲は -32767 〜 32767
+      for (let i = 0; i < outputArray.length; i++) {
+        normalizedOutputArray[i] = outputArray[i] / 0x7FFF;
+      }
+
+      // [Chrome] sampleRate: 48000, numberOfFrames: 480, format: `f32-planar`
+      // Insertable Streamsの対応ブラウザが増えたら可変にする必要がある
+      const format = 'f32-planar';
+      let outputAudioData = new AudioData({
+        format,
+        sampleRate: inputAudioData.sampleRate,
+        numberOfFrames: inputAudioData.numberOfFrames,
+        numberOfChannels: inputAudioData.numberOfChannels,
+        timestamp: inputAudioData.timestamp,
+        data: normalizedOutputArray
+      });
+      controller.enqueue(outputAudioData);
       inputAudioData.close();
 
     },
